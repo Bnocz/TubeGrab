54920c3cfa20a0546faccbdc88774cb8e98dbe9e
Bertram Nocon <BertNocon2@gmail.com>
2024-03-19 18:02:14 -0700

    adding xml layout variations

--- MagicTube/Models/DTO/VillaDTO.cs
+++ MagicTube/Models/DTO/VillaDTO.cs
@@ -1,11 +1,16 @@
 ﻿namespace MagicTube.Models.DTO
+using System.ComponentModel.DataAnnotations;
 {
     public class VillaDTO
     {
         public int Id { get; set; }
+        [Required]
+    [StringLength(30)]
+    public String Name { get; set; }
+    public DateTime CreatedDate { get; set; }
 
-        public String Name { get; set; }
+    public int Occupancy { get; set; }
 
-        public DateTime CreatedDate { get; set; }
-    }
+    public int Sqft { get; set; }
+}
 }
